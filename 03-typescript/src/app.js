f9f1e50b0e91150d9cc1b358b4a4fefe3f59fcef
Alberto Baigorria <lbaigorria@outlook.com>
2024-03-10 22:53:33 -0400

    new project typescritps

---
+++ 03-typescript/src/app.js
@@ -0,0 +1,25 @@
+const heroes = [
+    {
+        id:1,
+        name: "Iroman",
+        owner: "Marvel"
+    },
+    {
+        id:2,
+        name: "Spiderman",
+        owner: "Marvel"
+    },
+    {
+        id:3,
+        name: "Batman",
+        owner: "DC"
+    },
+]
+
+const findHeroById = (id) => {
+    return heroes.find((hero) => hero.id === id);
+}
+
+const hero = findHeroById(4);
+
+console.log(hero?.name ?? "Hero Not Found");
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # nodejscursone
 Curso de NodeJS de Cero a Experto por Fernando Herrera
 
+Proyectos:
+
+# 02-bases
+
 Paquetes:
 
 npm install -g nodemon
@@ -9,7 +13,13 @@ npm install --save get-age
 npm install axios
 npm install winstom --TS: Archivo Type Scritps
 
+# 03-typescripts
+
+npm init -y
+
 #  run
+
+npx nodemon src/app
 
 npm install
 npm run dev
