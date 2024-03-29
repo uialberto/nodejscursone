b34d499834e4b602069554f7a1cbcd521a610f31
Alberto Baigorria <lbaigorria@outlook.com>
2024-03-29 19:26:28 -0400

    Implementacion HTTP Server Express Route

--- 07-restweb/src/presentation/server.ts
+++ 07-restweb/src/presentation/server.ts
@@ -1,5 +1,6 @@
 
 import express from 'express';
+import path from 'path';
 
 export class Server {
 
@@ -14,6 +15,14 @@ export class Server {
         //* Public Folders
 
         this.app.use(express.static('public'));
+
+
+        this.app.get('*',(req,res)=>{
+
+            const indexPath = path.join(__dirname + '../../../public/index.html')
+            res.sendFile(indexPath);
+            return;
+        });
 
         this.app.listen(8082, () => {
             console.log(`Server running on port ${8082}`);
